3cc2b78ff72c3464acf96b04ac6c00e43a641631
Hans Goor <RootedDev@outlook.com>
2018-09-10 15:00:20 +0200

    Added pip install support

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="nsapipython",
+    version="9f9c7c0de",
+    description="A Python wrapper to the NS API.",
+    url="https://github.com/EyeDevelop/nsapi-python",
+    author="EyeDevelop (Hans Goor)",
+    license="GPLv3",
+    packages=["nsapipython"],
+    zip_safe=False,
+    install_requires=[
+        "requests",
+        "bs4"
+    ]
+)
